1baa422a449c32d62fa3ba6de4d669cea87e8fb6
Florent <59225340+FlorentGerbaud@users.noreply.github.com>
2023-10-10 00:43:24 +0200

    Update Modele1.py

--- Python/Modele1.py
+++ Python/Modele1.py
@@ -22,7 +22,7 @@ x2Pos[0] = 1.0  # Initial value for x2 as specified
 #def of the function
 
 def x2_prime(t):
-    return a2 * (x1Pos[t] - x2Pos[t]) + 800*x1Pos[t]
+    return a2 * (x1Pos[t] - x2Pos[t])
 
 def x1_prime():
     return 130 * (1000 / 3600)
