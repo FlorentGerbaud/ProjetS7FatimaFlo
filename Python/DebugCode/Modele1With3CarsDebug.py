3a1a75c05d19c6615e3133d472a5938f7a3a5b34
Florent <59225340+FlorentGerbaud@users.noreply.github.com>
2023-10-10 22:04:49 +0200

    Update Modele1With3CarsDebug.py

--- Python/DebugCode/Modele1With3CarsDebug.py
+++ Python/DebugCode/Modele1With3CarsDebug.py
@@ -1,50 +1,49 @@
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Check if the correct number of command-line arguments is provided
-if len(sys.argv) != 3:
-    print("Usage: python your_script_name.py a2 h")
-    sys.exit(1)
 
-# Parse the command-line arguments
-accident=False
-a2 = float(sys.argv[1])
-h = float(sys.argv[2])
 
-# Rest of your script remains the same
-# ...
-
+# Define the parameters
+a2 = 2.0  #capacity of acceleration deceleration
+a3= 1.0
+h = 1.0  # Time step
 T = 10.0  # Total simulation time
 n_steps = int(T / h)  # Number of time steps
 
 # Initialize arrays to store the results
 x1Pos = np.zeros(n_steps)
 x2Pos = np.zeros(n_steps)
+x3Pos = np.zeros(n_steps)
 time = np.zeros(n_steps)
+accident=False
 
 # Initial conditions
 time[0] = 0.0  # Initial time
 x1Pos[0] = 2.0  # Initial value for x1 as specified
 x2Pos[0] = 1.0  # Initial value for x2 as specified
+x3Pos[0] = 0.5  # Initial value for x2 as specified
 
 #def of the function
+
+def x1_prime():
+    return 130 * (1000 / 3600)
 
 def x2_prime(t):
     return a2 * (x1Pos[t] - x2Pos[t])
 
-def x1_prime():
-    return 130 * (1000 / 3600)
+def x3_prime(t):
+    return a3 * (x2Pos[t] - x3Pos[t])
 
 # Euler's explicit method
 for t in range(1, n_steps):
     
     x1Pos[t] = x1Pos[t-1] + x1_prime() * h
     x2Pos[t] = x2Pos[t-1] + x2_prime(t-1) * h
+    x3Pos[t] = x3Pos[t-1] + x3_prime(t-1) * h
     time[t] = time[t-1] + h
     if(x2Pos[t] >= x1Pos[t]):
         accident=True
-        break;
+        break
         
 print(t)
 
@@ -52,8 +51,9 @@ if(accident==True):
     plt.figure(figsize=(10, 6))
     plt.plot(time[0:t+1], x1Pos[0:t+1], label='x1(t)')
     plt.plot(time[0:t+1], x2Pos[0:t+1], label='x2(t)')
+    plt.plot(time[0:t+1], x3Pos[0:t+1], label='x3(t)')
     plt.xlabel('Time')
-    plt.ylabel('Values')
+    plt.ylabel('Distance')
     plt.legend()
     plt.title('Accident case')
     plt.grid(True)
@@ -63,9 +63,34 @@ else:
     plt.figure(figsize=(10, 6))
     plt.plot(time, x1Pos, label='x1(t)')
     plt.plot(time, x2Pos, label='x2(t)')
+    plt.plot(time, x3Pos, label='x3(t)')
     plt.xlabel('Time')
     plt.ylabel('Values')
     plt.legend()
     plt.title('Euler Explicit Method for x2\' = a2(x1(t) - x2(t)) with Constant x1\'')
     plt.grid(True)
     plt.show()
+
+
+# Initialize arrays to store the speeds of the cars
+v1 = np.zeros(n_steps)
+v2 = np.zeros(n_steps)
+v3 = np.zeros(n_steps)
+
+# Calculate speeds
+for t in range(1, n_steps):
+    v1[t] =  x1_prime()
+    v2[t] = (x2Pos[t] - x2Pos[t-1]) 
+    v3[t] = (x3Pos[t] - x3Pos[t-1]) 
+
+# Plot the speeds
+plt.figure(figsize=(10, 6))
+plt.plot(time, v1, label='Speed of x1(t)')
+plt.plot(time, v2, label='Speed of x2(t)')
+plt.plot(time, v3, label='Speed of x3(t)')
+plt.xlabel('Time')
+plt.ylabel('Speed')
+plt.legend()
+plt.title('Speed of Cars Over Time')
+plt.grid(True)
+plt.show()
