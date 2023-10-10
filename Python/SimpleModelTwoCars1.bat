4227c8954d82ed179c63e25733b9c01721c168c1
Florent <59225340+FlorentGerbaud@users.noreply.github.com>
2023-10-10 09:38:42 +0200

    .bat modification

--- Python/SimpleModelTwoCars1.bat
+++ Python/SimpleModelTwoCars1.bat
@@ -2,17 +2,17 @@
 setlocal enabledelayedexpansion
 
 :: Set default values for the arguments
-set "arg1=2.0"
-set "arg2=1.0"
+set "a2=2.0"
+set "h=1.0"
 
 :: Check if the first argument is provided and use it if available
-if not "%~1"=="" set "arg1=%~1"
+if not "%~1"=="" set "a2=%~1"
 
 :: Check if the second argument is provided and use it if available
-if not "%~2"=="" set "arg2=%~2"
+if not "%~2"=="" set "h=%~2"
 
 :: Run the Python script with the provided or default arguments
-python Modele1.py %arg1% %arg2%
+python Modele1.py %a2% %h%
 
 :: Exit the batch script
 exit /b
