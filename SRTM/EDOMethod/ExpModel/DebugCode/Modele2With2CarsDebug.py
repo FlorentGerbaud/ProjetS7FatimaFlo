7600a0011777c6333e99561d912aaab24e08c591
frharrour <fatimazahragharrour@gmail.com>
2023-11-05 14:28:21 +0100

    adding comments and simulation

--- SRTM/EDOMethod/ExpModel/DebugCode/Modele2With2CarsDebug.py
+++ SRTM/EDOMethod/ExpModel/DebugCode/Modele2With2CarsDebug.py
@@ -3,52 +3,56 @@ import matplotlib.pyplot as plt
 
 
 
-# Define the parameters
-lambda2 = 10.0  #capacity of acceleration deceleration
+#________________________________________Definition and initialization of the parameters________________________________________#
+
+lambda2 = 10.0  #Acceleration/Deceleration capacity
 h = 1.0  # Time step
 T = 50.0  # Total simulation time
 n_steps = int(T / h)  # Number of time steps
-d2=1.0 #security distance
-x2_prime_max=160*(1000/3600) #maximum speed of the second car
-accident= False
+d2=3.0 #Security distance
+x2_prime_max=160*(1000/3600) #Maximum speed of the second car
+accident=False #Flag to indicate if an accident has occurred
 
 # Initialize arrays to store the results
-x1= np.zeros(n_steps)
-x2= np.zeros(n_steps)
-time = np.zeros(n_steps)
-x1_prime_values = np.zeros(n_steps)
-x2_prime_values = np.zeros(n_steps)
-accident=False
+x1= np.zeros(n_steps) #Array to store the position of the first car
+x2= np.zeros(n_steps) #Array to store the position of the second car
+time = np.zeros(n_steps) #Array to store the time values
+x1_prime_values = np.zeros(n_steps) #Array to store the speed values of the first ca
+x2_prime_values = np.zeros(n_steps) #Array to store the speed values of the second car
+
 
 # Initial conditions
 time[0] = 0.0  # Initial time
 x1[0] = 3.0  # Initial value for x1 as specified
-x2[0] = 1.0  # Initial value for x2 as specified , we verify also that the security disstance is respected
+x2[0] = 1.0  # Initial value for x2 as specified, we also verify that the security distance is respected
 
+#________________________________________Definition of the functions________________________________________#
 
-#def of the function
+#Definition of the function to calculate x1's speed
 def x1_prime():
     return 110 *(1000/3600)
 
-#def of the fucntion x2_prime with the special model of exp
+#Definition of the function to calculate x2's speed with a special exponential model
 def x2_prime(t):
     return x2_prime_max*(1-np.exp((-lambda2/x2_prime_max)*(x1[t]-x2[t]-d2)))
 
 
 # Euler's explicit method
 for t in range(1, n_steps):
-    x1_prime_values[t] = x1_prime()  # Stocker les valeurs de vitesse de x1
-    x2_prime_values[t] = x2_prime(t-1)  # Stocker les valeurs de vitesse de x2
+    x1_prime_values[t] = x1_prime()  #Store the speed values of x1
+    x2_prime_values[t] = x2_prime(t-1)  #Store the speed values of x2
     x1[t] = x1[t-1] + x1_prime() * h
     x2[t] = x2[t-1] + x2_prime(t-1) * h
     time[t] = time[t-1] + h
-    if(x1[t]-x2[t]<= d2 ):
+    if(x1[t-1]-x2[t-1]<= d2 ): #Verify if the security distance is respected 
         accident=True
-        break;
+        print(t)
+        print(accident)
+        
     
 
-
-        
+#________________________________________Displaying the simulation results________________________________________#
+   
 # Display the results in side-by-side subplots
 fig, axes = plt.subplots(1, 2, figsize=(12, 4))
 
@@ -70,17 +74,6 @@ axes[1].legend()
 axes[1].set_title('Velocities')
 axes[1].grid(True)
 
+#Display the plots
 plt.show()
-
-
-
-
-
-
-
-
-   
-    
-        
-
 
--- SRTM/EDOMethod/ExpModel/DebugCode/Modele2with3CarsDebug.py
+++ SRTM/EDOMethod/ExpModel/DebugCode/Modele2with3CarsDebug.py
@@ -1,59 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#__________________________________________Definition and initialisation of parameters_____________________________________________#
-h=1.0 #time step
-T=50.0 #total simulation steps
-n_steps=int(T/h) #number of time steps
+#__________________________________________Definition and initialization of parameters_____________________________________________#
 
+h=1.0 #Time step
+T=50.0 #Total simulation steps
+n_steps=int(T/h) #Tumber of time steps
+accident=False
 
-#CAR 2
-lambda_2=10.0 #capacity of acceleration/deceleration of car 2 
-x2_prime_max=160*(1000/3600) #maximum speed of the car 2
-d2=1.0 #security distance
+#Parameters for car 2
+lambda_2=10.0 #Capacity of acceleration/deceleration 
+x2_prime_max=160*(1000/3600) #Maximum speed 
+d2=1.0 #Security distance
 
-#CAR 3
-lambda_3=5.0 #capacity of acceleration/deceleration of car 3
-x3_prime_max=180*(1000/3600) #maximum speed of car 3
-d3=2.0 #security distance
-
+#Parameters for car 3
+lambda_3=5.0 #Capacity of acceleration/deceleration 
+x3_prime_max=180*(1000/3600) #Maximum speed 
+d3=2.0 #Security distance
 
 #Positions: x1,x2 et x3
 x1=np.zeros(n_steps)
 x2=np.zeros(n_steps)
 x3=np.zeros(n_steps)
+
 #Initialisation of positions
 x1[0]=6.0
 x2[0]=4.0 #respecting d2
 x3[0]=1.0 #respecting d3
-#time 
+
+#Time 
 time=np.zeros(n_steps) 
 time[0]=0.0
+
 #Velocities: 
 x1_prime_values = np.zeros(n_steps)
 x2_prime_values = np.zeros(n_steps)
 x3_prime_values = np.zeros(n_steps)
+
 #_____________________________________________________Definition of functions_________________________________________________________#
 
+#Definition of x1's speed function
 def x1_prime():
     return 120*(1000/3600)
 
+# Definition of x2's speed function using a special exponential model
 def x2_prime(t):
     return x2_prime_max*(1-np.exp((-lambda_2/x2_prime_max)*(x1[t]-x2[t]-d2)))
 
+# Definition of x3's speed function using a special exponential model
 def x3_prime(t):
     return x3_prime_max*(1-np.exp((-lambda_3/x3_prime_max)*(x2[t]-x3[t]-d3)))
 
 #Euler implicite
 for t in range(1, n_steps):
-    x1_prime_values[t] = x1_prime()  # Stocker les valeurs de vitesse de x1
-    x2_prime_values[t] = x2_prime(t-1)  # Stocker les valeurs de vitesse de x2
-    x3_prime_values[t] = x3_prime(t-1)  # Stocker les valeurs de vitesse de x3
+    x1_prime_values[t] = x1_prime()  #Store the speed values of x1
+    x2_prime_values[t] = x2_prime(t-1)  #Store the speed values of x1
+    x3_prime_values[t] = x3_prime(t-1)  #Store the speed values of x1
 
     x1[t] = x1[t-1] + x1_prime() * h
     x2[t] = x2[t-1] + x2_prime(t-1) * h
     x3[t] = x3[t-1] + x3_prime(t-1) * h
     time[t] = time[t-1] + h
+    
     
 
 #__________________________________________Plot of solutions____________________________________________#
